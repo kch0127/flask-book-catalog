e2d836fef7b83f44f361a4ae61e077a5b0570c52
kch0127 <53538121+kch0127@users.noreply.github.com>
2019-07-31 19:27:14 -0700

    Update

--- config/dev.py
+++ config/dev.py
@@ -1,4 +1,4 @@
 DEBUG = True
-SECRET_KEY = '175326ff'
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:175326ff@localhost/catalog_db'
+SECRET_KEY = 'topsecret'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:topsecret@localhost/catalog_db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
